42b77d65a0e220bf1f5f42aa0234ee0cdf0460d8
ZybinSK <dvachhero@yandex.ru>
2023-12-07 15:26:01 +0500

    Добавил КИТ.Ментор

--- storage_game/tutorial/views.py
+++ storage_game/tutorial/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import KmbButton, KmbSubmenu, KmbContent
+from django.core.paginator import Paginator
 
 def kmb_view(request):
     buttons = KmbButton.objects.all()
@@ -10,9 +11,19 @@ def kmbsubmenu_view(request, button_id):
     return render(request, 'kmbsubmenu.html', {'submenus': submenus, 'button_id': button_id})
 
 def content_view(request, submenu_id):
-    contents = KmbContent.objects.filter(submenu_id=submenu_id)
+    contents_list = KmbContent.objects.filter(submenu_id=submenu_id)
+    paginator = Paginator(contents_list, 1)  # Показывать по 1 элементу на странице
+
+    page_number = request.GET.get('page', 1)  # Получить номер страницы из запроса
+    contents = paginator.get_page(page_number)
+
     submenu = KmbSubmenu.objects.get(id=submenu_id)
     button_id = submenu.parent_button_id
-    return render(request, 'content.html', {'contents': contents, 'button_id': button_id})
+
+    return render(request, 'content.html', {
+        'contents': contents,
+        'button_id': button_id,
+        'submenu_id': submenu_id
+    })
 
 
