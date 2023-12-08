37af47cb49a7d2a314c623e77da64a7dc3c5cfb4
ZybinSK <dvachhero@yandex.ru>
2023-12-08 10:32:11 +0500

    Добавил КИТ.Ментор

--- storage_game/tutorial/admin.py
+++ storage_game/tutorial/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import KmbButton, KmbSubmenu, KmbContent
 
-# Register your models here.
+class KmbButtonAdmin(admin.ModelAdmin):
+    list_display = ('title',)  # поля, которые будут отображаться в списке
+    search_fields = ('title',)  # поля, по которым можно искать
+
+admin.site.register(KmbButton, KmbButtonAdmin)
+
+class KmbSubmenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent_button')
+    search_fields = ('title', 'parent_button')
+    list_filter = ('parent_button',)  # фильтр для быстрого поиска по родительской кнопке
+
+admin.site.register(KmbSubmenu, KmbSubmenuAdmin)
+
+class KmbContentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'image_url', 'video_url', 'submenu_id')
+    search_fields = ('text', 'submenu_id',)
+    list_filter = ('text', 'submenu_id',)  # фильтр для быстрого поиска по родительской кнопке
+
+admin.site.register(KmbContent, KmbContentAdmin)
